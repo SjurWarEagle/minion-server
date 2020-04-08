a792ba8a9d83d6b50347e932f803d161720c0436
Torsten Kunkel <git@tkunkel.de>
2020-04-08 21:41:59 +0200

    * remove tmp-file for image * add test-page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,67 +1,70 @@
-import { Controller, Get, Header, Query, Res } from '@nestjs/common';
-import { createReadStream, mkdirSync } from 'fs';
-import { EventEmitter } from 'events';
-import { v4 } from 'uuid';
-import { SvgManipulationService } from './services/svg-manipulation.service';
-import { isNullOrUndefined } from 'util';
-import { DnaController } from './dna.controller';
+import {Controller, Get, Header, Query, Res} from '@nestjs/common';
+import {Readable} from 'stream';
+import {EventEmitter} from 'events';
+import {SvgManipulationService} from './services/svg-manipulation.service';
+import {isNullOrUndefined} from 'util';
+import {DnaController} from './dna.controller';
 
 const sharp = require('sharp');
 
 @Controller()
 export class AppController {
-  constructor() {}
 
-  @Get('/render/')
-  @Header('Content-Type', 'image/png')
-  async getRenderedMinion(
-    @Query('width') width: string,
-    @Query('height') height: string,
-    @Res() res,
-  ): Promise<any> {
-    const myWidth = !isNullOrUndefined(width) ? Number.parseInt(width) : 500;
-    const myHeight = !isNullOrUndefined(height) ? Number.parseInt(height) : 500;
-    return this.getRenderedMinionWithSize(myWidth, myHeight, res);
-  }
-
-  private async getRenderedMinionWithSize(
-    width: number,
-    height: number,
-    @Res() res,
-  ): Promise<any> {
-    mkdirSync('tmp', { recursive: true });
-    const outputFile = 'tmp/out-' + v4() + '.png';
-
-    // Event emitter only stops the program from terminating before the async function has been run
-    const finishEventEmitter = new EventEmitter();
-    finishEventEmitter.on('finish', () => {
-      // process.exit()
-    });
-
-    let svgManipulationService = new SvgManipulationService();
-    const svg = await svgManipulationService.applyDna(
-      await new DnaController().getDna(),
-    );
-
-    return run();
-
-    async function run() {
-      try {
-        await sharp(Buffer.from(svg), { density: 500 })
-          .resize(width, height, {
-            fit: 'contain',
-            kernel: 'mitchell',
-          })
-          .sharpen()
-          .png()
-          .toFile(outputFile);
-        const data = createReadStream(outputFile);
-        return data.pipe(res);
-      } catch (err) {
-        console.error(err);
-      } finally {
-        finishEventEmitter.emit('finish');
-      }
+    constructor() {
     }
-  }
+
+    @Get('/render/')
+    @Header('Content-Type', 'image/png')
+    async getRenderedMinion(
+        @Query('width') width: string,
+        @Query('height') height: string,
+        @Res() res,
+    ): Promise<any> {
+        const myWidth = !isNullOrUndefined(width) ? Number.parseInt(width) : 500;
+        const myHeight = !isNullOrUndefined(height) ? Number.parseInt(height) : 500;
+        return this.getRenderedMinionWithSize(myWidth, myHeight, res);
+    }
+
+    private async getRenderedMinionWithSize(
+        width: number,
+        height: number,
+        @Res() res,
+    ): Promise<any> {
+
+        // Event emitter only stops the program from terminating before the async function has been run
+        const finishEventEmitter = new EventEmitter();
+        finishEventEmitter.on('finish', () => {
+            // process.exit()
+        });
+
+        let svgManipulationService = new SvgManipulationService();
+        const svg = await svgManipulationService.applyDna(
+            await new DnaController().getDna(),
+        );
+
+        return run();
+
+        async function run() {
+            try {
+                const buf = await sharp(Buffer.from(svg), {density: 500})
+                    .resize(width, height, {
+                        fit: 'contain',
+                        kernel: 'mitchell',
+                    })
+                    .sharpen()
+                    .png()
+                    .toBuffer();
+
+                const stream = new Readable();
+                stream.push(buf);
+                stream.push(null);
+
+                return stream.pipe(res);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                finishEventEmitter.emit('finish');
+            }
+        }
+    }
 }
